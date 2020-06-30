c7a359ca31dbae165073e9440522a3f97a748a07
jatin0814 <201951073@iiitvadodara.ac.in>
2020-06-30 23:50:38 +0530

    srver

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client:'pg',
 	connection:{
-		host:'postgresql-clean-46866',
-		user:'postgres',
-		password:'',
-		database:'smart-brain'
+		host:process.env.DATABASE_URL,
+		ssl:true
 	}
 });
 
